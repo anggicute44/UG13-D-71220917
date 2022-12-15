45fd8df9fe086d7f0b0d5c6659a849fb47f839d6
anggicute44 <117901030+anggicute44@users.noreply.github.com>
2022-12-15 16:22:39 +0700

    Add files via upload

---
+++ UG13_D_71220917.py
@@ -0,0 +1,19 @@
+import turtle
+sc=turtle.Turtle()
+sc=turtle
+sc.bgcolor("purple")
+sc.dot(50)
+sc.pencolor(white)
+sc.pensize(100)
+sc.forward(100)
+sc.left(90)
+sc.up()
+sc.goto(-100,-100)
+sc.circle(90)
+
+
+
+
+
+
+sc= turtle.Screen().exitonclick() 
